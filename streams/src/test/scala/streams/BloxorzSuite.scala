cc987180f43428425a5eb62bdd8ca12e935b5580
Daniel Cukier <tech+danicuki@elo7.com>
2013-11-05 18:50:19 -0200

    Streams solved problem

--- streams/src/test/scala/streams/BloxorzSuite.scala
+++ streams/src/test/scala/streams/BloxorzSuite.scala
@@ -17,20 +17,21 @@ class BloxorzSuite extends FunSuite {
      * is a valid solution, i.e. leads to the goal.
      */
     def solve(ls: List[Move]): Block =
-      ls.foldLeft(startBlock) { case (block, move) => move match {
-        case Left => block.left
-        case Right => block.right
-        case Up => block.up
-        case Down => block.down
+      ls.foldLeft(startBlock) {
+        case (block, move) => move match {
+          case Left => block.left
+          case Right => block.right
+          case Up => block.up
+          case Down => block.down
+        }
       }
-    }
   }
 
   trait Level1 extends SolutionChecker {
-      /* terrain for level 1*/
+    /* terrain for level 1*/
 
     val level =
-    """ooo-------
+      """ooo-------
       |oSoooo----
       |ooooooooo-
       |-ooooooooo
@@ -42,14 +43,41 @@ class BloxorzSuite extends FunSuite {
 
   test("terrain function level 1") {
     new Level1 {
-      assert(terrain(Pos(0,0)), "0,0")
-      assert(!terrain(Pos(4,11)), "4,11")
+      assert(terrain(Pos(0, 0)), "0,0")
+      assert(terrain(Pos(1, 1)), "1,1")
+      assert(terrain(Pos(4, 9)), "4,9")
+      assert(!terrain(Pos(4, 10)), "4,10")
+      assert(!terrain(Pos(-1, -1)), "-1,-1")
+      assert(!terrain(Pos(5, 9)), "5,9")
+      assert(!terrain(Pos(0, 3)), "0,3")
+      assert(!terrain(Pos(6, 0)), "6,0")
     }
   }
 
   test("findChar level 1") {
     new Level1 {
-      assert(startPos == Pos(1,1))
+      assert(startPos == Pos(1, 1))
+      assert(goal == Pos(4, 7))
+    }
+  }
+
+  test("neighbors") {
+    new Level1 {
+      val n = neighborsWithHistory(Block(Pos(1, 1), Pos(1, 1)), List(Left, Up))
+      assert(n.toList(0) == (Block(Pos(1, 2), Pos(1, 3)), List(Right, Left, Up)))
+      assert(n.toList(1) == (Block(Pos(2, 1), Pos(3, 1)), List(Down, Left, Up)))
+    }
+  }
+
+  test("new neighbors") {
+    new Level1 {
+      val n = newNeighborsOnly(
+        Set(
+          (Block(Pos(1, 2), Pos(1, 3)), List(Right, Left, Up)),
+          (Block(Pos(2, 1), Pos(3, 1)), List(Down, Left, Up))).toStream,
+        Set(Block(Pos(1, 2), Pos(1, 3)), Block(Pos(1, 1), Pos(1, 1))))
+
+      assert(n.toList(0) == (Block(Pos(2, 1), Pos(3, 1)), List(Down, Left, Up)))
     }
   }
 
